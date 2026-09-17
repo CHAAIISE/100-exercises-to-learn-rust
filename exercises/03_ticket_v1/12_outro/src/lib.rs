091aa2183d50dcad217306cde184e90cd79273e9
CHAAIISE <190202299+CHAAIISE@users.noreply.github.com>
2026-09-17 17:46:33 +0200

    part 3 fini

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -6,6 +6,64 @@
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
 //
+pub struct Order {
+    product_name : String,
+    quantity : u64,
+    unit_price : u64,
+}
+
+impl Order {
+    pub fn new(product_name : String, quantity : u64, unit_price : u64) -> Order {
+        assert!(product_name != "", "Product name can't me empty");
+        assert!(product_name.len() < 300, "Product name can't be longer than 300 bytes");
+        assert!(quantity > 0, "Quantity must be greater than zero");
+        assert!(unit_price > 0, "Unit price must be greater than zero");
+        Order {
+            product_name, 
+            quantity, 
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u64 {
+        &self.quantity * &self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u64 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u64 {
+        &self.unit_price
+    }
+
+    pub fn set_quantity(&mut self, quantity : u64) -> &mut Order {
+        assert!(quantity > 0, "Quantity must be greater than zero");
+        self.quantity = quantity;
+        self
+    }
+
+    pub fn set_product_name(&mut self, product_name : String) -> &mut Order {
+        assert!(product_name != "", "Product name can't me empty");
+        assert!(product_name.len() < 300, "Product name can't be longer than 300 bytes");
+        self.product_name = product_name;
+        self
+    }
+
+    pub fn set_unit_price(&mut self, unit_price : u64) -> &mut Order {
+        assert!(unit_price > 0, "Unit price must be greater than zero");
+        self.unit_price = unit_price;
+        self
+    }
+
+}
+
+
+
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
 // Integration here has a very specific meaning: they test **the public API** of your project.
